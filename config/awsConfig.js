41b64fd9d101b40fb1649c8e640aa4a0d113fa0d
SavvyGaikwad <147950613+SavvyGaikwad@users.noreply.github.com>
2024-10-21 22:10:55 +0530

    Update awsConfig.js

--- config/awsConfig.js
+++ config/awsConfig.js
@@ -5,10 +5,9 @@ const AWS = require('aws-sdk');
 
 // Configure AWS
 // Configure the AWS SDK with your credentials
-AWS.config.update({
-    accessKeyId: 'AKIAZQ3DSF3KWMBOIRG3',       // Replace with your actual AWS Access Key ID
-    secretAccessKey: 'QqrH5owAnEZKGUeTAEstsFetkEK0JTKAc6L67DcO', // Replace with your actual AWS Secret Access Key
-    region: 'eu-north-1'                      // Your AWS region
+ region: 'eu-north-1', // Replace with your preferred region
+            accessKeyId: 'AKIAZQ3DSF3KTWAKBFVT', // Replace with your access key
+            secretAccessKey: 'xsdRFjfPZ+9XwuV8pFxgo0OzzJWaoQkDogGyw3LK                     // Your AWS region
 });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
